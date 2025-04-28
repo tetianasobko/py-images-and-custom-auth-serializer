dedafb31a19053b35df76dd8f9b7532b4d66db55
Tetiana Sobko <sobkotiana@gmail.com>
2025-04-28 23:42:38 +0300

    Fix issues

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -132,10 +132,6 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
 
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
